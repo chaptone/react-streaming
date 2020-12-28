fdfad9eaab9a381706daae6051ad13dfe36d0400
chaptone <rakkan.j@gmail.com>
2020-12-28 11:18:03 +0700

    Add custom history browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import { Layout } from "antd";
 
 import StreamCreate from "./streams/StreamCreate";
@@ -8,11 +8,12 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <Layout>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Layout.Content style={{ padding: "50px" }}>
           <Route path="/" exact component={StreamList} />
@@ -21,7 +22,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </Layout.Content>
-      </BrowserRouter>
+      </Router>
     </Layout>
   );
 };
