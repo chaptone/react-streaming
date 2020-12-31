de7cd452e750e43d7a759520963df204b78785cf
chaptone <rakkan.j@gmail.com>
2020-12-31 23:28:31 +0700

    Add rmtp streaming server

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,19 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 const StreamShow = () => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
   const { id } = useParams();
   const dispatch = useDispatch();
   const stream = useSelector(state => state.streams[id]);
   useEffect(() => {
     dispatch(fetchStream(id));
   }, []);
+
+  useEffect(() => {
+    buildPlayer();
+
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.destroy();
+      }
+    };
+  });
+
+  const buildPlayer = () => {
+    if (playerRef.current || !stream) {
+      return;
+    }
+    playerRef.current = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    playerRef.current.attachMediaElement(videoRef.current);
+    playerRef.current.load();
+  };
   return (
     <div>
       {stream && (
         <div>
+          <video ref={videoRef} style={{ width: "100%" }} controls />
           <h1>{stream.title}</h1>
           <h5>{stream.description}</h5>
         </div>
